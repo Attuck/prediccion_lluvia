5bc03dd3e1d95a41de024294d8bc4f78e68578b9
Ricardo Apú <rapuc@symbiotic.net>
2017-11-08 14:01:15 -0600

    cambios después de hablar con el profe

--- raw_to_average.py
+++ raw_to_average.py
@@ -1,26 +1,28 @@
 import csv
-from numpy import mean
-STATION_COUNT = {
-1:127799,
-2:264368,
-3:380367,
-4:439439,
-5:469243,
-6:499160
-}
+# from numpy import mean
+# STATION_COUNT = {
+# 1:127799,
+# 2:264368,
+# 3:380367,
+# 4:439439,
+# 5:469243,
+# 6:499160
+# }
 # STATION_COUNT = {
 # 1:19,
 # 2:83,
 # 3:99,
 # }
-STATION_TEST = {
-1:int(STATION_COUNT[1]*0.2),
-2:int((STATION_COUNT[2]-STATION_COUNT[1])*0.2),
-3:int((STATION_COUNT[3]-STATION_COUNT[2])*0.2),
-4:int((STATION_COUNT[4]-STATION_COUNT[3])*0.2),
-5:int((STATION_COUNT[5]-STATION_COUNT[4])*0.2),
-6:int((STATION_COUNT[6]-STATION_COUNT[5])*0.2)
-}
+# STATION_TEST = {
+# 1:int(STATION_COUNT[1]*0.5),
+# 2:int((STATION_COUNT[2]-STATION_COUNT[1])*0.5),
+# 3:int((STATION_COUNT[3]-STATION_COUNT[2])*0.5),
+# 4:int((STATION_COUNT[4]-STATION_COUNT[3])*0.5),
+# 5:int((STATION_COUNT[5]-STATION_COUNT[4])*0.5),
+# 6:int((STATION_COUNT[6]-STATION_COUNT[5])*0.5)
+# }
+total_rows= 499160
+test_proportion = 0.5
 COLUMNS = [
 'AIRTEMP_C_AVG',
 'AIRTEMP_C_MAX',
@@ -38,8 +40,8 @@ COLUMNS = [
 ]
 #DATA_DATE,hour,IDSTATION,AIRTEMP_C_AVG,AIRTEMP_C_MAX,AIRTEMP_C_MIN,RH_PERCENT,WINDSP_MS_S,WINDSP_MS_U,WINDDIR_DU,WINDDIR_SDU,BP_MBAR,SLR_W_AVG,SLR_W_MAX,SLR_W_MIN,SLR_W_STD,PAR_DEN_AVG,PAR_DEN_MAX,PAR_DEN_MIN,PAR_DEN_STD,RAIN_MM_TOT
 
-print(STATION_TEST)
-# filepath = '/Users/rapuc/Google Drive/UCR/2017-II/CI-1441_Paradigmas/prediccion_lluvia/datos_paradigmas_test_100.csv'
+print(total_rows)
+# filepath = 'datos_paradigmas_test_100.csv'
 filepath = 'datos_paradigmas_without_manual_limited.csv'
 # filepath =  'datos_paradigmas_testing.csv'
 with open(filepath, newline='') as source:
@@ -48,59 +50,39 @@ with open(filepath, newline='') as source:
             reader = csv.DictReader(source)
             output_data = csv.writer(data_dest, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
             output_test = csv.writer(test_dest, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
-            hold = None
-            last_avg_row = []
+            classified_row = []
             count = 0
-            station = 1
+            # station = 1
+            test_rows = total_rows*test_proportion
+            test_modulo = int(total_rows/test_rows)
             for row in reader:
                 # print(count)
-                if count == STATION_COUNT[station] :
-                    station = station+1
-                    last_avg_row.append('f')
-                    output_data.writerow(last_avg_row)
-                    last_avg_row = []
-                    hold = None
-                if hold == None:
-                    hold = row
-                else:
-                    if hold['RAIN_MM_TOT'] == "" and row['RAIN_MM_TOT'] == "":
-                        rain_mm = "?"
-                    elif hold['RAIN_MM_TOT'] == "":
-                        rain_mm =float(row['RAIN_MM_TOT'])
-                    elif row['RAIN_MM_TOT'] == "":
-                        rain_mm =float(hold['RAIN_MM_TOT'])
+                # if count == STATION_COUNT[station] :
+                    # station = station+1
+                for column in COLUMNS:
+                    if row[column] == "" or row[column] == None:
+                        classified_row.append("?")
                     else:
-                        try:
-                            rain_mm = float(hold['RAIN_MM_TOT']) +float(row['RAIN_MM_TOT'])
-                        except Exception:
-                            print(hold['RAIN_MM_TOT'])
-                            print(row['RAIN_MM_TOT'])
-                            print(count)
-                            rain_mm = 0.0
-                            pass
-                    if last_avg_row != []:
-                        if rain_mm == "?" or rain_mm ==0:
-                            last_avg_row.append('f')
-                        else :
-                            last_avg_row.append('t')
-                        if count < STATION_COUNT[station]-STATION_TEST[station]:
-                            output_data.writerow(last_avg_row)                      
-                            last_avg_row = []
-                        else:
-                            output_test.writerow(last_avg_row)
-                            last_avg_row = []
+                        classified_row.append(float(row[column]))
+                
+                try:
+                    rain = float(row['RAIN_MM_TOT'])
+                except Exception as identifier:
+                    rain = 0.0
+                    pass
+                if  rain > 0.0:
+                    classified_row.append('t')
+                else:
+                    classified_row.append('f')
 
-                    for column in COLUMNS:
-                        if hold[column] == "" and row[column] == "":
-                             last_avg_row.append("?")
-                        elif hold[column] == "":
-                            last_avg_row.append(float(row[column]))
-                        elif row[column] == "":
-                            last_avg_row.append(float(hold[column]))
-                        else:
-                            last_avg_row.append(mean([float(hold[column]),float(row[column])]))
-                    last_avg_row.append(rain_mm )
-                    hold=None
+                if count % test_modulo == 0:
+                    output_test.writerow(classified_row)
+                    classified_row = []
+                    # print('one to test')
+                else:                    
+                    output_data.writerow(classified_row)  
+                    classified_row = []  
+                    # print('one to data')
+
+                    
                 count = count +1
-            last_avg_row.append('f')
-            output_data.writerow(last_avg_row)
